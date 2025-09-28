000d8959faa006463ade99ea041a5caeddc23653
Sean Ippolito <ippolitosean@gmail.com>
2025-09-27 22:15:34 -0700

    Successfully migrated new models

---
+++ app/db/alembic/versions/c9a2b90abf48_init_users_accounts_transactions.py
@@ -0,0 +1,20 @@
+"""init users, accounts, transactions
+
+Revision ID: c9a2b90abf48
+Revises: 
+Create Date: 2025-09-27 22:09:32.303418
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'c9a2b90abf48'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    pass
+
+def downgrade() -> None:
+    pass
